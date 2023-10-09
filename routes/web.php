8f86792356d98c59c8108ea5d7ec01c5bec1fd15
mores-hitt <fernando.sanabria@alumnos.ucn.cl>
2023-10-09 12:30:38 -0300

    Creacion vista upload
    Se crea la vista para subir el excel junto a la logica para que se muestre por pantalla al logearse un administrador

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,15 @@ Route::get('login', function () {
     return view('auth.login');
 })->name('login');
 
+
+Route::middleware('auth')->group(function () {
+
+});
+
 Route::post('login', [AuthController::class, 'login'])->name('auth.login');
 
 Route::post('logout', [AuthController::class, 'logout'])->name('logout');
+
+Route::get('upload-files', function () {
+    return view('admin_routes.index');
+})->name('upload');
