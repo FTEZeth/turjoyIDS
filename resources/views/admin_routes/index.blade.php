eb3c30f4470db195121f73de015db468536dce7d
Erik Maluenda <114884171+soberlewa12@users.noreply.github.com>
2023-10-10 23:38:05 -0300

    branch loca xd

--- resources/views/admin_routes/index.blade.php
+++ resources/views/admin_routes/index.blade.php
@@ -9,9 +9,8 @@
     <form class ="flex flex-col items-center w-1/2" action="#" method="POST" enctype="multipart/form-data">
         @csrf
         <div>
-            <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white" for="file_input">Upload file</label>
             <input name="document" class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" aria-describedby="file_input_help" id="file_input" type="file">
-            <p class="mt-1 text-sm text-gray-500 dark:text-gray-300" id="file_input_help">SVG, PNG, JPG or GIF (MAX. 800x400px).</p>
+            <p class="mt-1 text-sm text-gray-500 dark:text-gray-300" id="file_input_help"> Tipo de archivo soportado: .xlsx (5mb).</p>
             @error('document')
                 <p class="bg-red-400 font-semibold my-4 text-lg text-center text-red-800 px-4 py-3 rounded-lg">
                     {{$message}}
