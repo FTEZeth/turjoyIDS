64e4dac66fa33e8f1f9ff6f4ff66908fd09b1f20
Erik Maluenda <erik.maluenda@alumnos.ucn.cl>
2023-11-01 13:17:00 -0300

    fix: Menu del administrador
    Ya estan corregido los fallos en el frontend del menu

--- resources/views/admin_routes/menu.blade.php
+++ resources/views/admin_routes/menu.blade.php
@@ -5,25 +5,24 @@
 @endsection
 
 @section('content')
-    <div class="flex items-center justify-center h-screen">
-        <h1 style="color: #0474DA;">>Bienvenido(a) nuevamente Administrador</h1>
+    <div class="flex flex-col items-center justify-center h-screen">
+        <h1 class="text-lg; text-100xl; font-bold" style="color: #0474DA; margin-bottom: 60px; font-size: 60px;">Bienvenido(a)
+            nuevamente Administrador
+        </h1>
         <div class="bg-gray-200 p-6 mx-auto rounded-lg lg:w-1/4" style="background-color: #eaeaea">
-            <a class="font-bold text-2xl text-center text-blue-500 uppercase mb-4" style="color: #0474DA;"
-                href="#">Cargar Rutas</a>
-            <a class="font-bold text-2xl text-center text-blue-500 uppercase mb-4" style="color: #0474DA;"
-                href="#">Realizar Reserva
-            </a>
-            <a class="font-bold text-2xl text-center text-blue-500 uppercase mb-4" style="color: #0474DA;"
-                href="#">Buscar Reserva
-            </a>
-            <a class="font-bold text-2xl text-center text-blue-500 uppercase mb-4" style="color: #0474DA;"
-                href="#">Ver Reporte de Reserva
-            </a>
+            <div class="text-white hover:cursor-pointer hover:bg-emerald-800 font-medium rounded-lg text-sm w-full p-3 text-center dark:bg-emerald-600 dark:hover:bg-emerald-700 dark:focus:ring-emerald-800"
+                style="background-color: #0474DA; margin-bottom: 20px;" href="#">Cargar Rutas</div>
+            <div class="text-white hover:cursor-pointer hover:bg-emerald-800 font-medium rounded-lg text-sm w-full p-3 text-center dark:bg-emerald-600 dark:hover:bg-emerald-700 dark:focus:ring-emerald-800"
+                style="background-color: #0474DA; margin-bottom: 20px;" href="#">Realizar Reserva</div>
+            <div class="text-white hover:cursor-pointer hover:bg-emerald-800 font-medium rounded-lg text-sm w-full p-3 text-center dark:bg-emerald-600 dark:hover:bg-emerald-700 dark:focus:ring-emerald-800"
+                style="background-color: #0474DA; margin-bottom: 20px;" href="#">Buscar Reserva</div>
+            <div class="text-white hover:cursor-pointer hover:bg-emerald-800 font-medium rounded-lg text-sm w-full p-3 text-center dark.bg-emerald-600 dark.hover:bg-emerald-700 dark.focus:ring-emerald-800"
+                style="background-color: #0474DA;" href="#">Ver Reporte de Reserva</div>
         </div>
 
-        <div class="mb-12 mx-auto">
-            <a class="px-6 py-3 bg-red-500 hover:bg-red-700 transition-all text-white font-semibold rounded-lg",
-                style="background-color: #FF6B6B" href="{{ route('logout') }}">Cerrar sesion </a>
+        <div class="mt-12 mx-auto">
+            <a class="px-6 py-3 bg-red-500 hover:bg-red-700 transition-all text-white font-semibold rounded-lg"
+                style="background-color: #FF6B6B" href="{{ route('home') }}">Volver</a>
         </div>
     </div>
 @endsection
