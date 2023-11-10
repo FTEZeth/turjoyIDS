587a11915cd0b961fc0b41392bd4fa97f03559cb
mores-hitt <fernando.sanabria@alumnos.ucn.cl>
2023-11-10 12:15:56 -0300

    feat: agrega validacion de campo de fecha

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -25,10 +25,10 @@
                     </select>
                 </div>
 
-                <!-- Dropdown for Date - might need to change to another form type -->
+                <!-- Dropdown for Date -->
                 <div class="flex items-center space-x-2 flex-1">
                     <img src="images/calendar-days.png" alt="Date icon" class="w-6 h-6 self-center">
-                    <input type="date" id="date" name="date" min="2023-01-01" max="2023-12-31" class="block w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline">
+                    <input type="date" id="date" name="date" min="{{ date('Y-m-d', strtotime('+0 day'))}}" max="2023-12-31" class="block w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline">
                 </div>
 
                 <!-- Dropdown for Seats -->
