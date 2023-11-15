bd7fd24051efe9624234ff3a9c61211ab9694912
mores-hitt <fernando.sanabria@alumnos.ucn.cl>
2023-11-15 12:44:22 -0300

    fix: arregla color de boleta

--- resources/views/client/order-success.blade.php
+++ resources/views/client/order-success.blade.php
@@ -63,7 +63,7 @@
                             </td>
                         </tr>
 
-                        <tr class="bg-cyan-100 border-b border-cyan-500"></tr>
+                        <tr class="bg-cyan-100 border-b border-cyan-500">
                             <th scope="row"
                                 class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                                 Fecha de la compra
